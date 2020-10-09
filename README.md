0d750bac6c4ab7000c32fcc7c662b87e2faa55bd
Burak Ozdemir <burakozdemir@protonmail.ch>
2020-10-09 15:10:07 +0300

    v3.0, renamed package and added audio export feature.

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# [Morse Code Translator](https://morsedecoder.com) with Audio - Morsify
+# [Morse Code Translator](https://morsedecoder.com) with Audio - Morse Decoder
 
-[![npm-version]][npm] [![npm-downloads]][npm] [![travis-ci]][travis]
+[![npm-version]][npm] [![npm-downloads]][npm]
 
-Morsify is a Morse code encoder and decoder with no dependencies. Currently, it supports Latin, Cyrillic, Greek, Hebrew, Arabic,
+Morse code encoder and decoder with no dependencies. It supports Latin, Cyrillic, Greek, Hebrew, Arabic,
 Persian, Japanese, Korean, and Thai, with audio-generation functionality using the [Web Audio API](https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API).
 
 ## Installation
@@ -10,23 +10,23 @@ Persian, Japanese, Korean, and Thai, with audio-generation functionality using t
 ### npm
 
 ```bash
-$ npm install morsify --save
+$ npm install morse-decoder --save
 ```
 
 ### yarn
 
 ```bash
-$ yarn add morsify
+$ yarn add morse-decoder
 ```
 
 ## Usage
 
 ```js
-const morsify = require('morsify');
-const encoded = morsify.encode('SOS'); // ... --- ...
-const decoded = morsify.decode('... --- ...'); // SOS
-const characters = morsify.characters(); // {'1': {'A': '.-', ...}, ..., '11': {'ㄱ': '.-..', ...}}
-const audio = morsify.audio('SOS');
+const morse = require('morse-decoder');
+const encoded = morse.encode('SOS'); // ... --- ...
+const decoded = morse.decode('... --- ...'); // SOS
+const characters = morse.characters(); // {'1': {'A': '.-', ...}, ..., '11': {'ㄱ': '.-..', ...}}
+const audio = morse.audio('SOS');
 audio.play(); // play audio
 audio.stop(); // stop audio
 audio.exportWave(); // download audio wave file (promise)
@@ -34,27 +34,7 @@ const url = await audio.getWaveUrl(); // get audio wave url (promise)
 const blob = await audio.getWaveBlob(); // get audio wave blob (promise)
 ```
 
-Alternatively, you can use the library directly with including the source file.
-
-```html
-<script src="https://rawgit.com/ozdemirburak/morsify/master/dist/morsify.min.js"></script>
-<script>
-    var encoded = morsify.encode('SOS'); // ... --- ...
-    var decoded = morsify.decode('... --- ...'); // SOS
-    var characters = morsify.characters(); // {'1': {'A': '.-', ...}, ..., '11': {'ㄱ': '.-..', ...}}
-    var audio = morsify.audio('SOS');
-    var oscillator = audio.oscillator; // OscillatorNode
-    var context = audio.context; // AudioContext
-    var gainNode = audio.gainNode; // GainNode
-    audio.play(); // play audio
-    audio.stop(); // stop audio
-    audio.exportWave(); // download audio wave file (promise)
-    var url = await audio.getWaveUrl(); // get audio wave url (promise)
-    var blob = await audio.getWaveBlob(); // get audio wave blob (promise)
-</script>
-```
-
-## Options and Localization
+### Options and localization
 
 You can customize the dash, dot, or space characters and specify the alphabet with the priority option for
 an accurate encoding and decoding.
@@ -77,12 +57,12 @@ Set the priority option according to the list below.
 - 12 => Thai
 
 ```js
-const cyrillic = morsify.encode('Ленинград', { priority: 5 }); // .-.. . -. .. -. --. .-. .- -..
-const greek = morsify.decode('... .- --. .- .--. .--', { priority: 6 }); // ΣΑΓΑΠΩ
-const hebrew = morsify.decode('.. ––– . –––', { dash: '–', dot: '.', priority: 7 }); // יהוה
-const japanese = morsify.encode('NEWS', { priority: 10, dash: '－', dot: '・', separator: '　' }); // －・　・　・－－　・・・
-const characters = morsify.characters({ dash: '–', dot: '•' }); // {'1': {'A': '•–', ...}, ..., '11': {'ㄱ': '•–••', ...}}
-const arabicAudio = morsify.audio('البراق', { // generates the Morse .- .-.. -... .-. .- --.- then generates the audio from it
+const cyrillic = morse.encode('Ленинград', { priority: 5 }); // .-.. . -. .. -. --. .-. .- -..
+const greek = morse.decode('... .- --. .- .--. .--', { priority: 6 }); // ΣΑΓΑΠΩ
+const hebrew = morse.decode('.. ––– . –––', { dash: '–', dot: '.', priority: 7 }); // יהוה
+const japanese = morse.encode('NEWS', { priority: 10, dash: '－', dot: '・', separator: '　' }); // －・　・　・－－　・・・
+const characters = morse.characters({ dash: '–', dot: '•' }); // {'1': {'A': '•–', ...}, ..., '11': {'ㄱ': '•–••', ...}}
+const arabicAudio = morse.audio('البراق', { // generates the Morse .- .-.. -... .-. .- --.- then generates the audio from it
   unit: 0.1, // period of one unit, in seconds, 1.2 / c where c is speed of transmission, in words per minute
   fwUnit: 0.1, // period of one Farnsworth unit to control intercharacter and interword gaps
   oscillator: {
@@ -117,9 +97,7 @@ $ gulp
 ## License
 The MIT License (MIT). Please see [License File](LICENSE) for more information.
 
-  [npm-version]: https://img.shields.io/npm/v/morsify.svg?style=flat-square
-  [npm-downloads]: https://img.shields.io/npm/dm/morsify.svg?style=flat-square
-  [travis-ci]: https://img.shields.io/travis/ozdemirburak/morsify/master.svg?style=flat-square
+  [npm-version]: https://img.shields.io/npm/v/morse-decoder.svg?style=flat-square
+  [npm-downloads]: https://img.shields.io/npm/dm/morse-decoder.svg?style=flat-square
 
-  [npm]: https://www.npmjs.com/package/morsify
-  [travis]: https://travis-ci.org/ozdemirburak/morsify
+  [npm]: https://www.npmjs.com/package/morse-decoder
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@
   } else {
     root[name] = factory();
   }
-})('morsify', this, () => {
+})('morse-decoder', this, () => {
   'use strict';
 
   const characters = {
@@ -217,57 +217,53 @@
     }
 
     return [timings, time];
-  }
+  };
 
-  const floatTo16BitPCM = (output, offset, input) => {
-    for (let i = 0; i < input.length; i++, offset += 2) {
-        let s = Math.max(-1, Math.min(1, input[i]));
-        output.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7FFF, true);
-    }
-  }
-
-  const writeString = (view, offset, string) => {
-    for (let i = 0; i < string.length; i++) {
-        view.setUint8(offset + i, string.charCodeAt(i));
-    }
-  }
-
-  // graciously borrowed from: https://github.com/mattdiamond/Recorderjs/blob/master/src/recorder.js
+  // Source: https://github.com/mattdiamond/Recorderjs/blob/master/src/recorder.js#L155
   const encodeWAV = (sampleRate, samples) => {
     let buffer = new ArrayBuffer(44 + samples.length * 2);
     let view = new DataView(buffer);
-
-    /* RIFF identifier */
+    const writeString = (view, offset, string) => {
+      for (let i = 0; i < string.length; i++) {
+        view.setUint8(offset + i, string.charCodeAt(i));
+      }
+    };
+    // RIFF identifier
     writeString(view, 0, 'RIFF');
-    /* RIFF chunk length */
+    // RIFF chunk length
     view.setUint32(4, 36 + samples.length * 2, true);
-    /* RIFF type */
+    // RIFF type
     writeString(view, 8, 'WAVE');
-    /* format chunk identifier */
+    // format chunk identifier
     writeString(view, 12, 'fmt ');
-    /* format chunk length */
+    // format chunk length
     view.setUint32(16, 16, true);
-    /* sample format (raw) */
+    // sample format (raw)
     view.setUint16(20, 1, true);
-    /* channel count */
+    // channel count
     view.setUint16(22, 1, true);
-    /* sample rate */
+    // sample rate
     view.setUint32(24, sampleRate, true);
-    /* byte rate (sample rate * block align) */
+    // byte rate (sample rate * block align)
     view.setUint32(28, sampleRate * 4, true);
-    /* block align (channel count * bytes per sample) */
+    // block align (channel count * bytes per sample)
     view.setUint16(32, 2, true);
-    /* bits per sample */
+    // bits per sample
     view.setUint16(34, 16, true);
-    /* data chunk identifier */
+    // data chunk identifier
     writeString(view, 36, 'data');
-    /* data chunk length */
+    // data chunk length
     view.setUint32(40, samples.length * 2, true);
-
+    // to PCM
+    const floatTo16BitPCM = (output, offset, input) => {
+      for (let i = 0; i < input.length; i++, offset += 2) {
+        let s = Math.max(-1, Math.min(1, input[i]));
+        output.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7FFF, true);
+      }
+    };
     floatTo16BitPCM(view, 44, samples);
-
     return view;
-  }
+  };
 
   const audio = (text, opts, morseString) => {
     let AudioContext = null;
@@ -275,10 +271,11 @@
     let context = null;
     let offlineContext = null;
     let source;
+
     const options = getOptions(opts);
     const morse = morseString || encode(text, opts);
     const [gainValues, totalTime] = getGainTimings(morse, options);
-    
+
     if (AudioContext === null && typeof window !== 'undefined') {
       AudioContext = window.AudioContext || window.webkitAudioContext;
       context = new AudioContext();
@@ -287,8 +284,8 @@
     }
 
     if (OfflineAudioContext === null && typeof window !== 'undefined') {
-      OfflineAudioContext = window.OfflineAudioContext;
-      offlineContext = new OfflineAudioContext(1, 22050 * totalTime, 22050);
+      OfflineAudioContext = window.OfflineAudioContext || window.webkitOfflineAudioContext;
+      offlineContext = new OfflineAudioContext(1, 44100 * totalTime, 44100);
     }
 
     const oscillator = offlineContext.createOscillator();
@@ -296,23 +293,23 @@
 
     oscillator.type = options.oscillator.type;
     oscillator.frequency.value = options.oscillator.frequency;
-    
+
     gainValues.forEach(([value, time]) => gainNode.gain.setValueAtTime(value, time));
 
     oscillator.connect(gainNode);
     gainNode.connect(offlineContext.destination);
     source.onended = options.oscillator.onended;
 
-    // offline rendering as a promise inspired by: http://joesul.li/van/tale-of-no-clocks/
+    // Inspired by: http://joesul.li/van/tale-of-no-clocks/
     let render = new Promise(resolve => {
       oscillator.start(0);
       offlineContext.startRendering();
       offlineContext.oncomplete = (e) => {
         source.buffer = e.renderedBuffer;
         resolve();
-      }
+      };
     });
-    
+
     let timeout;
 
     const play = async () => {
@@ -330,21 +327,20 @@
     const getWaveBlob = async () => {
       await render;
       const waveData = encodeWAV(offlineContext.sampleRate, source.buffer.getChannelData(0));
-      const audioBlob = new Blob([waveData], { 'type': 'audio/wav' });
-      return audioBlob;
-    }
+      return new Blob([waveData], { 'type': 'audio/wav' });
+    };
 
     const getWaveUrl = async () => {
       const audioBlob = await getWaveBlob();
       return URL.createObjectURL(audioBlob);
     };
 
-    const exportWave = async () => {
+    const exportWave = async (filename) => {
       let waveUrl = await getWaveUrl();
-      var anchor = document.createElement('a');
+      let anchor = document.createElement('a');
       anchor.href = waveUrl;
       anchor.target = '_blank';
-      anchor.download = 'morsify.wav';
+      anchor.download = filename || 'morse.wav';
       anchor.click();
     };
 
