a95df23e7a5d5fc099187e2f6c08e3649306da77
Drishit Mitra <drishitmitra@gmail.com>
2022-10-01 22:21:11 +0530

    Migrate Code to TypeScript

---
+++ src/index.d.ts
@@ -0,0 +1,24 @@
+interface Window {
+  webkitAudioContext: typeof AudioContext
+  webkitOfflineAudioContext: typeof OfflineAudioContext
+}
+
+interface Characters { [key: string]: { [key:string ]: string } }
+
+interface Oscillator {
+  type?: OscillatorType;
+  frequency?: number;
+  onended?: (this: AudioScheduledSourceNode, ev: Event) => any | null;
+}
+
+interface Options {
+  dash: string;
+  dot: string;
+  space: string;
+  separator: string;
+  invalid: string;
+  priority: number;
+  unit: number;
+  fwUnit: number;
+  oscillator: Oscillator
+}
