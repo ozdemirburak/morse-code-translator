1b2801c49b8b2aea5160bd375b996c278b775400
Drishit Mitra <drishit96@users.noreply.github.com>
2022-10-03 14:10:02 +0530

    Migrate Code to TypeScript (#22)
    Migrated code to TypeScript, removed jshint and added eslint.

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,14 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const minify = require('gulp-babel-minify');
 const concat = require('gulp-concat');
+const ts = require("gulp-typescript");
+const tsProject = ts.createProject("tsconfig.json");
 
 gulp.task('default', () =>
-  gulp.src('src/index.js')
+  tsProject
+    .src()
+    .pipe(tsProject())
+    .js
     .pipe(babel({presets: ['@babel/env']}))
     .pipe(minify())
     .pipe(concat('morse-decoder.min.js'))
