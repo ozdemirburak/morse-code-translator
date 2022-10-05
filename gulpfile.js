57b473133adb1f6e3d10cc8e4d8f478755cb7e29
Chris Jones <chris@ctjones.net>
2022-10-05 22:12:57 +1100

    split typescript file fix some eslint complaints remove circular dependency between characters and options

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,15 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const minify = require('gulp-babel-minify');
 const concat = require('gulp-concat');
-const ts = require("gulp-typescript");
-const tsProject = ts.createProject("tsconfig.json");
+const ts = require('gulp-typescript');
+const tsProject = ts.createProject('tsconfig.json');
 
 gulp.task('default', () =>
   tsProject
     .src()
     .pipe(tsProject())
     .js
-    .pipe(babel({presets: ['@babel/env']}))
+    .pipe(babel({ presets: ['@babel/env'] }))
     .pipe(minify())
     .pipe(concat('morse-decoder.min.js'))
     .pipe(gulp.dest('dist'))
---
+++ src/options.ts
@@ -0,0 +1,22 @@
+const getOptions = (opts: Partial<Options>): Options => {
+  const options: Options = {
+    ...opts,
+    dash: opts.dash || '-',
+    dot: opts.dot || '.',
+    space: opts.space || '/',
+    separator: opts.separator || ' ',
+    invalid: opts.invalid || '#',
+    priority: opts.priority || 1,
+    unit: opts.unit || 0.08, // period of one unit, in seconds, 1.2 / c where c is speed of transmission, in words per minute
+    fwUnit: opts.fwUnit || opts.unit || 0.08, // Farnsworth unit to control intercharacter and interword gaps
+    oscillator: {
+      ...opts.oscillator,
+      type: opts.oscillator.type || 'sine', // sine, square, sawtooth, triangle
+      frequency: opts.oscillator.frequency || 500, // value in hertz
+      onended: opts.oscillator.onended || null // event that fires when the tone has stopped playing
+    }
+  };
+  return options;
+};
+
+export default getOptions;
